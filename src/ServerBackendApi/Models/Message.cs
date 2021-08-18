d268350cd79c6996cbc1eabc6999eff1ccc17e8e
Marko Marković <68012781+bbv-mmarkovic@users.noreply.github.com>
2021-08-18 08:01:57 +0200

    Small clean up.

--- src/ServerBackendApi/Models/Message.cs
+++ src/ServerBackendApi/Models/Message.cs
@@ -5,8 +5,11 @@
     public class Message
     {
         public string clientuniqueid { get; set; }
+
         public string type { get; set; }
+
         public string message { get; set; }
+
         public DateTime date { get; set; }
     }
 }
